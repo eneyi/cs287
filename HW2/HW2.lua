5ec669ff5fd5c581eeb084586964d25447b2d4a3
nicodri <nicodrizard@hotmail.com>
2016-02-23 08:48:52 -0500

    update

--- HW2/HW2.lua
+++ HW2/HW2.lua
@@ -1,12 +1,20 @@
+-- Documentation:
+-- ---- How to call it from the command line?
+-- For example:
+-- $ th HW1.lua -datafile SST1.hdf5 -classifier linear_svm
+-- Other argument possible (see below)
+-- 
+-- ---- Is there an Output?
+-- By default, the predictions on the test set are saved in hdf5 format as classifier .. opt.f
+
 -- Only requirements allowed
 require("hdf5")
 require("nn")
-require("optim")
 
 cmd = torch.CmdLine()
 
 -- Cmd Args
-cmd:option('-datafile', 'PTB.hdf5', 'pre-processed data file')
+cmd:option('-datafile', 'data/PTB.hdf5', 'pre-processed data file')
 cmd:option('-classifier', 'nb', 'classifier to use')
 cmd:option('-f', '_pred_test.f5', 'File name for the predictions on the test')
 
@@ -14,18 +22,18 @@ cmd:option('-f', '_pred_test.f5', 'File name for the predictions on the test')
 -- The parameters are defined one by one, depending on the classifier call
 
 -- Common hyperparameters for the gradient descent
-cmd:option('-eta', '0.01', 'gradient descent learning rate parameter')
-cmd:option('-batch_size', '20', 'size of the mini-batch for the stochastic gradient descent')
-cmd:option('-ep_max', '5', 'number of epoch (i.e. updates of the gradient by record) for the stochastic gradient descent')
+cmd:option('-eta', 0.01, 'gradient descent learning rate parameter')
+cmd:option('-batch_size', 20, 'size of the mini-batch for the stochastic gradient descent')
+cmd:option('-ep_max', 5, 'number of epoch (i.e. updates of the gradient by record) for the stochastic gradient descent')
 
 -- Hyperparameters for the Neural Network
-cmd:option('-embedding', '1', 'boolean value to use the embeddings to initiliaze the lookup table (the dim hidden 1 parameter won\'t be considered if set to true')
-cmd:option('-dim_hidden1', '50', 'Dimension of the first linear layer in the NN')
-cmd:option('-dim_hidden2', '50', 'Dimension of the first linear layer in the NN')
+cmd:option('-embedding', 1, 'boolean value to use the embeddings to initiliaze the lookup table (the dim hidden 1 parameter won\'t be considered if set to true')
+cmd:option('-dim_hidden1', 50, 'Dimension of the first linear layer in the NN')
+cmd:option('-dim_hidden2', 50, 'Dimension of the first linear layer in the NN')
 
 
 -- Hyperparameters for the Naive Bayes
-cmd:option('-alpha', '0.5', 'smoothing parameter')
+cmd:option('-alpha', 0.5, 'smoothing parameter')
 
 
 --------------------------
@@ -101,7 +109,6 @@ function predict_NB(Xword, Xcap, prior, x_conditional_y_word, x_conditional_y_ca
             end
         end
         accuracy = right / n
-        print("Accuracy: " .. accuracy)
         return Ypred, accuracy
     else
         return Ypred
@@ -115,35 +122,33 @@ end
 function define_log_reg(nwords, ncap, nclasses)
     log_reg = nn.Sequential()
     --Include the lookup tables
-    par = nn.ParallelTable()
-    par:add(nn.LookupTable(5*nwords, nclasses)) -- first child
-    par:add(nn.LookupTable(5*ncap,nclasses)) 
-
+    par = nn.LookupTable(nwords + ncap,nclasses)
     log_reg:add(par)
-    log_reg:add(nn.Sum(2))
+    log_reg:add(nn.View(1,-1,10*nclasses))
+    log_reg:add(nn.Squeeze()) -- this layer is to go from a 1xAxB tensor to AxB dimensional tensor (https://groups.google.com/forum/#!topic/torch7/u4OEc0GB74k)
     -- Add the bias b
-    log_reg:add(nn.Add(nclasses))
+    log_reg:add(nn.Add(10*nclasses))
     log_reg:add(nn.LogSoftMax())
 
     return log_reg
+end
 
 --------------------------
 -- Neural Network
 --------------------------
 
-function compute_accuracy(pred, true_)
-    max,argmax = pred:max(2)
-    acc = 0
-    for i = 1, true_:size(1) do
-        if argmax[i][1] == true_[i] then
-            acc = acc + 1
-        end
-    end
-    score = acc/true_:size(1)
-    
-    return score
+local Squeeze, parent = torch.class('nn.Squeeze', 'nn.Module')
+
+function Squeeze:updateOutput(input)
+    self.size = input:size()
+    self.output = input:squeeze()
+    return self.output
 end
 
+function Squeeze:updateGradInput(input, gradOutput)
+  self.gradInput = gradOutput:view(self.size)
+  return self.gradInput  
+end
 
 -- If words_embeddings is nil, weight are initialized randomly by torch
 function define_nn(nwords, ncap, nclasses, dim_hidden, dim_hidden2, word_embeddings)
@@ -168,48 +173,62 @@ function define_nn(nwords, ncap, nclasses, dim_hidden, dim_hidden2, word_embeddi
     return neuralnet_wc
 end
 
-function train(train_output, train_input_word_windows, train_input_cap_windows, neuralnet_wc, ep_max, eta)
-    train_new = torch.cat(train_input_word_windows,
-        torch.add(train_input_cap_windows, 100002),2)
+--------------------------
+-- Helper
+--------------------------
+
+function train(train_input, train_output, model, ep_max, eta)
     -- Formating the dataset to train
     dataset={};
-    for i=1,train_new:size(1) do 
-      dataset[i] = {train_new[i]:view(1,10), train_output[i]}
+    for i=1,train_input:size(1) do 
+      dataset[i] = {train_input[i]:view(1,10), train_output[i]}
     end
-    function dataset:size() return train_new:size(1) end -- 100 examples
+    function dataset:size() return train_input:size(1) end -- 100 examples
 
     criterion = nn.ClassNLLCriterion()
 
     -- Training
     timer = torch.Timer()
-    trainer = nn.StochasticGradient(neuralnet_wc, criterion)
+    trainer = nn.StochasticGradient(model, criterion)
     trainer.learningRate = eta
     trainer.maxIteration = ep_max
-
     trainer:train(dataset)
 
-    return neuralnet_wc
+    return model
 end
 
-function pred_nn(input_word_windows, input_cap_windows, neuralnet_wc, output)
-    new = torch.cat(input_word_windows,
-        torch.add(input_cap_windows, 100002),2)
-    pred_val = neuralnet_wc:forward(new)
+function pred(input, model, output)
+    pred_val = model:forward(input)
     if output then
         valid_accuracy = compute_accuracy(pred_val, output)
-        return pred_val, accuracy
+        return pred_val, valid_accuracy
     end
     return pred_val
+end
+
+function compute_accuracy(pred, true_)
+    max,argmax = pred:max(2)
+    acc = 0
+    for i = 1, true_:size(1) do
+        if argmax[i][1] == true_[i] then
+            acc = acc + 1
+        end
+    end
+    score = acc/true_:size(1)
+    
+    return score
 end
 
 function main() 
     -- Parse input params
     opt = cmd:parse(arg)
     local f = hdf5.open(opt.datafile, 'r')
-    train_input_word_windows = f:read('train_input_word_windows'):all()
-    train_input_cap_windows = f:read('train_input_cap_windows'):all()
-    valid_input_word_windows = f:read('valid_input_word_windows'):all()
-    valid_input_cap_windows = f:read('valid_input_cap_windows'):all()
+    train_word = f:read('train_input_word_windows'):all()
+    train_cap = f:read('train_input_cap_windows'):all()
+    valid_word = f:read('valid_input_word_windows'):all()
+    valid_cap = f:read('valid_input_cap_windows'):all()
+    test_word = f:read('test_input_word_windows'):all()
+    test_cap = f:read('test_input_cap_windows'):all()
     train_output = f:read('train_output'):all()
     valid_output = f:read('valid_output'):all()
     word_embeddings = f:read('word_embeddings'):all()
@@ -222,17 +241,15 @@ function main()
     eta = opt.eta
     ep_max = opt.ep_max
 
-    if ((classifier == 'nb') || (classifier == 'log_reg')) then
+    if ((classifier == 'nb') or (classifier == 'log_reg')) then
         -- Process the features for NB and log reg
+        nwords_bis = nwords * 5
+        ncap_bis = ncap * 5
         for j = 1, 5 do
             train_word:narrow(2,j,1):add((j-1)*nwords)
             valid_word:narrow(2,j,1):add((j-1)*nwords)
-        end
-        for j = 1, 5 do
             train_cap:narrow(2,j,1):add((j-1)*ncap)
             valid_cap:narrow(2,j,1):add((j-1)*ncap)
-        end
-        for j = 1, 5 do
             test_word:narrow(2,j,1):add((j-1)*ncap)
             test_cap:narrow(2,j,1):add((j-1)*ncap)
         end
@@ -246,37 +263,61 @@ function main()
             prior, x_conditional_y_word, x_conditional_y_cap = NaiveBayes(train_word, train_cap, train_output, nwords_bis, ncap_bis, nclasses, alpha)
             -- Prediction on the validation set
             Ypred_validate, accuracy_validate = predict_NB(valid_word, valid_cap, prior, x_conditional_y_word, x_conditional_y_cap, nclasses, valid_output)
+            print("Accuracy on validation: " .. accuracy)
             -- Prediction on the test set
             Testpred = predict_NB(test_word, test_cap, prior, x_conditional_y_word, x_conditional_y_cap, nclasses)
 
             print('Time elapsed for NB ' .. timer:time().real .. ' seconds',"\n")
+        else
+            timer = torch.Timer()
+            log_reg = define_log_reg(nwords_bis, ncap_bis, nclasses)
+            new_train = torch.cat(train_word,
+                torch.add(train_cap, nwords),2)
+            log_reg = train(new_train, train_output, log_reg, ep_max, eta)
+
+            -- Accuracy on validation set
+            new_validation = torch.cat(valid_word,
+                torch.add(valid_cap, nwords),2)
+            pred_accuracy, validation_accuracy = pred(new_validation, log_reg, valid_output)
+            print("Accuracy on validation: " .. validation_accuracy)
+
+            -- Prediction on test
+            new_test = torch.cat(test_word,
+                torch.add(test_cap, nwords),2)
+            Testpred = pred(new_test, log_reg)
+            print('Time elapsed for NB ' .. timer:time().real .. ' seconds',"\n")
+
         end
     elseif (classifier == 'nn') then
+        timer = torch.Timer()
         -- Setting the parameters
         dim_hidden1 = opt.dim_hidden1
         dim_hidden2 = opt.dim_hidden2
 
+        new_train = torch.cat(train_word,
+            torch.add(train_cap, 100002),2)
         if opt.embeddings then
             neuralnet_wc = define_nn(nwords, ncap, nclasses, dim_hidden1, dim_hidden2, word_embeddings)
         else
             neuralnet_wc = define_nn(nwords, ncap, nclasses, dim_hidden1, dim_hidden2, word_embeddings)
         end
-        neuralnet_wc = train_nn(train_output, train_input_word_windows, train_input_cap_windows, neuralnet_wc, ep_max, eta)
+        neuralnet_wc = train(new_train, train_output, neuralnet_wc, ep_max, eta)
 
         -- Accuracy on validation set
-        new_validation = torch.cat(valid_input_word_windows,
-            torch.add(valid_input_cap_windows, nwords),2)
-        validation_accuracy = compute_accuracy(neuralnet_wc:forward(valid_new), valid_output)
+        new_validation = torch.cat(valid_word,
+            torch.add(valid_cap, nwords),2)
+        pred_accuracy, validation_accuracy = pred(new_validation, neuralnet_wc, valid_output)
         print("Accuracy on validation: " .. validation_accuracy)
 
         -- Prediction on test
-        new_test = torch.cat(test_input_word_windows,
-            torch.add(test_input_cap_windows, nwords),2)
-        Testpred = neuralnet_wc:forward(new_test)
+        new_test = torch.cat(test_word,
+            torch.add(test_cap, nwords),2)
+        Testpred = pred(new_test, neuralnet_wc)
+        print('Time elapsed for NB ' .. timer:time().real .. ' seconds',"\n")
     end
 
     -- Saving the predictions on test
-    filename = classifier .. opt.f
+    filename = 'data/' .. classifier .. opt.f
     if (filename) then
         myFile = hdf5.open(filename, 'w')
         myFile:write('Testpred', Testpred)
