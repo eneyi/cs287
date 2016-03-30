91b9347729aae58cd97b4ea1fe34fb6bcfabf09c
Virgile Audi <vaudi@g.harvard.edu>
2016-03-29 22:28:23 -0400

    Greedy Done + submission NN

--- HW4/greedy_nn.lua
+++ HW4/greedy_nn.lua
@@ -2,44 +2,84 @@ require 'nn';
 require 'hdf5';
 require 'neuralnetpart1.lua';
 
-myFile = hdf5.open('data_preprocessed/5-grams.hdf5','r')
+function predict_space(nnlm, dat):
+
+	local dat_nospace = dat
+	local it = 1
+	local i = 1
+	local nextpred = torch.Tensor(2)
+
+	while it<data['input_data_dat_nospace_nospace']:size(1)-(Nwin-1) do
+		it = it + 1
+		nextpred:copy(nnlm:forward(dat_nospace:narrow(1,i,Nwin)));
+		m, argm = nextpred:max(1)
+
+		if argm[1] == 2 then
+			i = i + 1		
+		elseif argm[1] == 1 then 
+			local dat_nospace_ = torch.LongTensor(dat_nospace:size(1)+1)
+			dat_nospace_:narrow(1,1,i+(Nwin-1)):copy(dat_nospace:narrow(1,1,i+(Nwin-1)))
+			dat_nospace_[i+Nwin] = 1
+			dat_nospace_:narrow(1,i+(Nwin-1)+2,dat_nospace:size(1)-i-(Nwin-1)):copy(dat_nospace:narrow(1,i+(Nwin-1)+1,dat_nospace:size(1)-i-(Nwin-1)))
+			dat_nospace = dat_nospace_
+			i = i + 2
+		end 
+	end
+	return dat_nospace
+end
+
+function predict_kaggle(dat_space):
+	local num_sent = 0
+
+	for i = 5,dat_space:size(1) do
+    	if dat_space[i] == 2 then
+        	num_sent = num_sent + 1
+    	end
+	end
+
+	local num_spaces = torch.DoubleTensor(num_sent,2)
+	local row = 1
+	local count_space = 0
+
+	for i=5,dat_space:size(1) do
+	    if dat_space[i] == 2 then
+	        num_spaces[{row, 1}] = row
+	        num_spaces[{row, 2}] = count_space
+	        count_space = 0
+	        row = row + 1
+	    elseif dat_space[i] == 1 then
+	        count_space = count_space + 1
+	    end
+	end
+
+	return num_spaces
+end
+
+myFile = hdf5.open('data_preprocessed/6-grams.hdf5','r')
 data = myFile:all()
 myFile:close()
 
-Nwin = 4
+Nwin = 5
 
 train_input = data['input_matrix_train']
 train_output = data['output_matrix_train']
 
+valid = data['input_data_valid_nospace']:clone()
+
+test = data['input_data_test']:clone()
+
+torch.manualSeed(1)
+
 nnlm1, crit = build_model(Nwin, 49, 2, 20, 80)
 
 train_model(train_input, train_output, nnlm1, crit, Nwin, 2, 0.01, 28, 10)
-valid = data['input_data_valid_nospace']:clone()
 
-it = 1
-i = 1
-nextpred = torch.Tensor(2)
+valid_space = predict_space(nnlm1, valid)
+num_spaces_valid = predict_kaggle(valid_space)
 
-print('here')
-while it<data['input_data_valid_nospace']:size(1)-(Nwin-1) do
-	if it % 50 == 0 then
-		print( it..'/'..data['input_data_valid_nospace']:size(1))
-	end
-	it = it + 1
-	nextpred:copy(nnlm1:forward(valid:narrow(1,i,Nwin)));
-	m, argm = nextpred:max(1)
+test_space = predict_space(nnlm1, test)
+num_spaces_test = predict_kaggle(test_space)
 
-	if argm[1] == 2 then
-		i = i + 1		
-	elseif argm[1] == 1 then 
-		valid_ = torch.LongTensor(valid:size(1)+1)
-		valid_:narrow(1,1,i+(Nwin-1)):copy(valid:narrow(1,1,i+(Nwin-1)))
-		valid_[i+Nwin] = 1
-		valid_:narrow(1,i+(Nwin-1)+2,valid:size(1)-i-(Nwin-1)):copy(valid:narrow(1,i+(Nwin-1)+1,valid:size(1)-i-(Nwin-1)))
-		valid = valid_
-		i = i + 2
-	end 
-end
-
-print(valid:narrow(1,1,25))
-print(data['input_data_valid']:narrow(1,1,30))
+myFile = hdf5.open('../submission/pred_test_greedy_nn_5', 'w')
+myFile:write('num_spaces', num_spaces_test)
+myFile:close()
